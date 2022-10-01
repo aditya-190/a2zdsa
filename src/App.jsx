3c82c0162f2fb21da5730a7b7c372ea785916125
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-10-01 15:52:35 +0530

    We will use localstorage as our main database.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,57 @@
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import DSA from './components/index.jsx'
 import initialData from './components/initialData.js'
 
 function App() {
-    let data = null
-    const fetchData = localStorage.getItem('data')
+    const [data, setData] = useState(null)
 
-    fetchData === null
-        ? localStorage.setItem('data', JSON.stringify(initialData))
-        : (data = JSON.parse(fetchData))
+    useEffect(() => {
+        const fetchData = localStorage.getItem('data')
+        fetchData === null
+            ? localStorage.setItem('data', JSON.stringify(initialData))
+            : setData(JSON.parse(fetchData))
+    }, [])
 
     return (
-        <Routes>
-            <Route
-                path="/"
-                element={
-                    <DSA
-                        data={data}
-                        isHomeScreen={true}
-                        selectedContentIndex={0}
-                        is404={false}
-                    />
-                }
-            />
-            {data.content.map((contentData, index) => {
-                return (
-                    <Route
-                        key={index}
-                        path={contentData.contentPath}
-                        element={
-                            <DSA
-                                data={data}
-                                isHomeScreen={false}
-                                selectedContentIndex={index}
-                                is404={false}
-                            />
-                        }
-                    />
-                )
-            })}
-            <Route
-                path="*"
-                element={<DSA data={data} isHomeScreen={true} is404={true} />}
-            />
-        </Routes>
+        data !== null && (
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <DSA
+                            data={data}
+                            isHomeScreen={true}
+                            selectedContentIndex={0}
+                            is404={false}
+                        />
+                    }
+                />
+                {data.content.map((contentData, index) => {
+                    return (
+                        <Route
+                            key={index}
+                            path={contentData.contentPath}
+                            element={
+                                <DSA
+                                    data={data}
+                                    isHomeScreen={false}
+                                    selectedContentIndex={index}
+                                    is404={false}
+                                />
+                            }
+                        />
+                    )
+                })}
+                <Route
+                    path="*"
+                    element={
+                        <DSA data={data} isHomeScreen={true} is404={true} />
+                    }
+                />
+            </Routes>
+        )
     )
 }
 
