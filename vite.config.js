d2b716deb526ef8c05edbe683a0c0e77b0ccf952
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-10-02 17:30:31 +0530

    Some build config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react'
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
 })
